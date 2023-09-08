650331f699e2dd6c292a7919c57b6b709997a300
s h a u n <73394707+ShaunDaniel@users.noreply.github.com>
2023-09-08 23:41:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3000
 
 
 dotenv.config()
